7327fd3d3162e86b7cb1cda40f3f42ee72ff6689
Theo-Shlamo <87035324+Theo-Shlamo@users.noreply.github.com>
2023-05-16 12:34:03 -0300

    Update libasm.asm
    my bad, it was actually rax
    
    return registers  modificated
    
    mov rax, 0 not needed and parameters returned in rdi in x64 not rax

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -27,82 +27,66 @@ cpuVendor:
 	pop rbp
 	ret
 
-; return registers functions FIJARSE LA PARTE DE ARMADO DE STACK Y SI HACE FALTA EL MOV RAX, 0
+; return registers functions PREGUNTAR LA PARTE DE ARMADO DE STACK
 getRax:
-
 	ret
 
 getRbx:
-	mov rax, 0
 	mov rax, rbx
 	ret
 
 getRcx:
-	mov rax, 0
 	mov rax, rcx
 	ret
 
 getRdx:
-	mov rax, 0
 	mov rax, rdx
 	ret
 
 getRsi:
-	mov rax, 0
 	mov rax, rsi
 	ret
 
 getRdi:
-	mov rax, 0
 	mov rax, rdi
 	ret
 
 getRbp:
-	mov rax, 0
 	mov rax, rbp
 	ret
 
 getRsp:
-	mov rax, 0
 	mov rax, rsp
 	ret
 
 getR8:
-	mov rax, 0
 	mov rax, r8
 	ret
 
 getR9:
-	mov rax, 0
 	mov rax, r9
 	ret
 
 getR10:
-	mov rax, 0
 	mov rax, r10
 	ret
 
 getR11:
-	mov rax, 0
 	mov rax, r11
 	ret
 	
 getR12:
-	mov rax, 0
 	mov rax, r12
 	ret
 
 getR13:
-	mov rax, 0
 	mov rax, r13
 	ret
 
 getR14:
-	mov rax, 0
 	mov rax, r14
 	ret
 	
 getR15:
-	mov rax, 0
 	mov rax, r15
 	ret
