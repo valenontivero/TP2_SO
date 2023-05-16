6c79583459fb2ae4aeada50b976b354530ca3add
Theo-Shlamo <87035324+Theo-Shlamo@users.noreply.github.com>
2023-05-16 12:07:03 -0300

    Update libasm.asm
    get registers functions without stackframe

--- Kernel/asm/libasm.asm
+++ Kernel/asm/libasm.asm
@@ -1,4 +1,5 @@
 GLOBAL cpuVendor
+GLOBAL getRax, getRbx, getRcx, getRdx, getRsi, getRdi, getRbp, getRsp, getR8, getR9, getR10, getR11, getR12, getR13, getR14, getR15
 
 section .text
 	
@@ -24,4 +25,84 @@ cpuVendor:
 
 	mov rsp, rbp
 	pop rbp
+	ret
+
+; return registers functions FIJARSE LA PARTE DE ARMADO DE STACK Y SI HACE FALTA EL MOV RAX, 0
+getRax:
+
+	ret
+
+getRbx:
+	mov rax, 0
+	mov rax, rbx
+	ret
+
+getRcx:
+	mov rax, 0
+	mov rax, rcx
+	ret
+
+getRdx:
+	mov rax, 0
+	mov rax, rdx
+	ret
+
+getRsi:
+	mov rax, 0
+	mov rax, rsi
+	ret
+
+getRdi:
+	mov rax, 0
+	mov rax, rdi
+	ret
+
+getRbp:
+	mov rax, 0
+	mov rax, rbp
+	ret
+
+getRsp:
+	mov rax, 0
+	mov rax, rsp
+	ret
+
+getR8:
+	mov rax, 0
+	mov rax, r8
+	ret
+
+getR9:
+	mov rax, 0
+	mov rax, r9
+	ret
+
+getR10:
+	mov rax, 0
+	mov rax, r10
+	ret
+
+getR11:
+	mov rax, 0
+	mov rax, r11
+	ret
+	
+getR12:
+	mov rax, 0
+	mov rax, r12
+	ret
+
+getR13:
+	mov rax, 0
+	mov rax, r13
+	ret
+
+getR14:
+	mov rax, 0
+	mov rax, r14
+	ret
+	
+getR15:
+	mov rax, 0
+	mov rax, r15
 	ret
