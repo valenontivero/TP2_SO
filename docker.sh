51871290d4cb92fda81579478c834e845a41cdbb
EmiN364 <68005487+EmiN364@users.noreply.github.com>
2023-05-20 20:18:24 -0300

    Create docker.sh

---
+++ docker.sh
@@ -0,0 +1,12 @@
+# Check if docker arqui exists
+if [ ! "$(docker ps -a -q -f name=arqui)" ]; then
+    # Create docker arqui
+    docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name arqui agodio/itba-so:1.0
+fi
+
+docker start arqui
+docker exec -it arqui make clean -C/root/Toolchain
+docker exec -it arqui make clean -C/root/
+docker exec -it arqui make -C/root/Toolchain
+docker exec -it arqui make -C/root/
+docker stop arqui
