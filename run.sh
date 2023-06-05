b614f626724818fd77335b533ea4551ef4a475b9
EmiN364 <68005487+EmiN364@users.noreply.github.com>
2023-06-04 23:47:53 -0300

    Add GDB

--- run.sh
+++ run.sh
@@ -7,4 +7,8 @@ else
     audio="pa"
 fi
 
-qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev $audio,id=audio0 -machine pcspk-audiodev=audio0
+if [[ "$1" = "gdb" ]]; then
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev $audio,id=audio0 -machine pcspk-audiodev=audio0 -rtc base=localtime -d int -s -S
+else
+    qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev $audio,id=audio0 -machine pcspk-audiodev=audio0 -rtc base=localtime
+fi
