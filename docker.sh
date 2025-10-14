2986aed27b21541ac7a88fda5e202b465baf5385
valenontivero <vontivero@itba.edu.ar>
2025-10-14 02:22:10 -0300

    fix docker compile

--- docker.sh
+++ docker.sh
@@ -1,13 +1,20 @@
-# Check if docker arqui exists
-if [ ! "$(docker ps -a -q -f name=arqui)" ]; then
-    # Create docker arqui
-    docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name arqui agodio/itba-so:1.0
+#!/bin/bash
+
+# Check if docker so_tp2 exists
+if [ ! "$(docker ps -a -q -f name=so_tp2)" ]; then
+    # Create docker so_tp2
+    docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name so_tp2 agodio/itba-so-multi-platform:3.0
 fi
 
-docker start arqui
-docker exec -it arqui make clean -C/root/Toolchain
-docker exec -it arqui make clean -C/root/
-docker exec -it arqui make -C/root/Toolchain
-docker exec -it arqui env GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01" make -C/root/
-docker stop arqui
+docker start so_tp2
+docker exec -it so_tp2 make clean -C/root/Toolchain
+docker exec -it so_tp2 make clean -C/root/
+docker exec -it so_tp2 make -C/root/Toolchain
+if [[ "$1" = "buddy" ]]; then
+    docker exec -it so_tp2 env GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01" make buddy -C/root/
+else
+    docker exec -it so_tp2 env GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01" make -C/root/
+fi
+docker exec -it so_tp2 chmod -R g+wx /root/
+# docker stop so_tp2
 bash run.sh
